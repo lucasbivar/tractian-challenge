207a3dc24bf95822cf511167aa70adb2a3fac424
Lucas Bivar <lucasbivarfonseca@gmail.com>
2023-08-23 01:56:18 -0300

    fix: vercel error

--- src/components/UnitCard/index.tsx
+++ src/components/UnitCard/index.tsx
@@ -15,7 +15,7 @@ export const UnitCard = ({name, image}: UnitCardProps) => {
   const navigate = useNavigate();
 
   return (
-    <Flex onClick={(e) => {
+    <Flex onClick={() => {
        navigate('/units/1')}} width={{base: "100%", sm: "47%", md: "47%", lg: "30%", xl: "31%"}} height="200px" position="relative" _hover={{boxShadow: "dark-lg"}} bg="#D7D7D7" cursor="pointer" borderRadius={10}>
       <Image fit="cover" width="100%" zIndex="2" src={image}  borderRadius={10}/>
       <Box zIndex="3" width="100%" height="100%"  borderRadius={10} position="absolute" bg="linear-gradient(0deg, rgba(26,48,113,1) 0%, rgba(255,255,255,0) 50%);" />
