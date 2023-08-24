4bccd427e334c4e730ecb5f7e61f9f1d4b557be8
Lucas Bivar <lucasbivarfonseca@gmail.com>
2023-08-24 01:45:18 -0300

    feat: adding wrapper card in companies list

--- src/pages/Companies/index.tsx
+++ src/pages/Companies/index.tsx
@@ -7,6 +7,7 @@ import {
 import { Navigate } from "react-router-dom";
 import { CompanyListItem } from "../../components/CompanyListItem";
 import { SetInfoModal } from "../../components/SetInfoModal";
+import { Card } from "../../components/Card";
 
 export const Companies = () => {
   const { isOpen: isOpenEdit, onOpen: onOpenEdit, onClose: onCloseEdit } = useDisclosure();
@@ -32,16 +33,19 @@ export const Companies = () => {
         </InputGroup>
         <Button onClick={onOpenEdit} mt={{base: "4", sm: "0"}} leftIcon={<FiPlus />} width={{base: "100%", sm: "45%", md: "38%", lg: "28%", xl: "18%"}} _hover={{bg: "#12255d", color: "#FFF"}}  color="#FFF" bg="#1A3071">Create Company</Button>
       </Flex>
-      <Flex flexDirection="column" justifyContent="space-between" flexWrap="wrap" width="100%" mb="50px">
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-        <CompanyListItem name="TDG Company" id={1}/>
-      </Flex>
+      <Card noPadding width="100%">
+        <Flex flexDirection="column" flexWrap="wrap"  paddingX="5" width="100%" mb="-1px">
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+          <CompanyListItem name="TDG Company" id={1}/>
+        </Flex>
+      </Card>
+
       <SetInfoModal view="new" type="company" isOpen={isOpenEdit} onClose={onCloseEdit} />
     </Flex>
   );
