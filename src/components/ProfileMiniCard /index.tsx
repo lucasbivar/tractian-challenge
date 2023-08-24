e6e6187a7600199a39cd828f615a8f12626cd25c
Lucas Bivar <lucasbivarfonseca@gmail.com>
2023-08-24 01:51:05 -0300

    fix: removing redirect when click in unit's profile card

--- src/components/ProfileMiniCard /index.tsx
+++ src/components/ProfileMiniCard /index.tsx
@@ -1,5 +1,4 @@
 import { Avatar, Flex, Text } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
 
 interface ProfileMiniCardProps {
   name?: string,
@@ -7,10 +6,9 @@ interface ProfileMiniCardProps {
   id?: number
 };
 
-export const ProfileMiniCard = ({name, email, id}: ProfileMiniCardProps) => {
-  const navigate = useNavigate();
+export const ProfileMiniCard = ({name, email}: ProfileMiniCardProps) => {
   return (
-    <Flex flexDirection="column" onClick={() => navigate(`/users/${id}`)} justifyContent="center" alignItems="center" paddingX="5px" paddingY="25px" width={{base: "100%", sm: "47%", md: "47%", lg: "30%", xl: "18%"}} border="1px solid #D7D7D7" _hover={{boxShadow: "base"}} bg="#FFF" borderRadius={10}>
+    <Flex flexDirection="column" justifyContent="center" alignItems="center" paddingX="5px" paddingY="25px" width={{base: "100%", sm: "47%", md: "47%", lg: "30%", xl: "18%"}} border="1px solid #D7D7D7" _hover={{boxShadow: "base"}} bg="#FFF" borderRadius={10}>
       <Avatar bg="#1A3071" color="#FFF" name={name} />
       <Text as="b" fontSize="md">
         {name}
